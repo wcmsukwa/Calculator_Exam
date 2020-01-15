3448ff98c93ab185c256464bd18e12d823c26e78
wcmsukwa <wcmsukwa@gmail.com>
2020-01-15 16:12:48 +0200

    Task: Statistical Operation

---
+++ StatOperationsTests/StatOperationsTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatOperations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatOperations.Tests
+{
+    [TestClass()]
+    public class StatOperationsTests
+    {
+        [TestMethod()]
+        public void MeanTest()
+        {
+            double[] a = { 10.45, 20.5, 10, 20.1, 15.5 };
+            Assert.AreEqual(15.31, StatOperations.Mean(a));
+        }
+
+        [TestMethod()]
+        public void MedianTest()
+        {
+            double[] a = { 10.45, 20.5, 10, 20.1, 15.5 };
+            Assert.AreEqual(3, StatOperations.Median(a));
+        }
+
+
+        [TestMethod()]
+        public void ModeTest()
+        {
+            double[] a = { 15.50, 20.5, 10, 20.1, 15.5 };
+            Assert.AreEqual(15.5, StatOperations.Mode(a));
+        }
+    }
+}
