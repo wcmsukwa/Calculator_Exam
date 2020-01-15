047c0bc3ff2cb4f9d338bc60794c4676f4dca280
gmachalo <57180690+gmachalo@users.noreply.github.com>
2020-01-15 16:56:39 +0200

    Feature: Added Basic calculator

---
+++ Root/Root.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Ops;
+
+namespace Root
+{
+    public interface IRoot
+    {
+        public dynamic Root(dynamic a);
+    }
+
+    public class Rooting : IRoot
+
+    {
+        public dynamic result;
+
+        public dynamic Root(dynamic a)
+        {
+            result = SquareRoot.Root(a);
+            return result;
+        }
+    }
+}
