e79c952386e237794c0216d4f3c200bbcda04982
wcmsukwa <wcmsukwa@gmail.com>
2020-01-16 11:47:08 +0200

    Revert "Revert "Merge pull request #7 from wcmsukwa/development""
    This reverts commit 4a2b50b2df3f0960c650fd3689fb970862a697a4.

---
+++ BasicCalculatorTests/CalcTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicCalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculator.Tests
+{
+    [TestClass()]
+    public class CalcTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calc calculator = new Calc();
+
+            int c = calculator.Add(1, 2);
+
+            Assert.AreEqual(3, c);
+            Assert.AreEqual(3, calculator.result);
+        }
+
+        [TestMethod()]
+        public void DivTest()
+        {
+            Calc calculator = new Calc();
+
+            int c = calculator.Divide(2, 1);
+
+            Assert.AreEqual(2, c);
+            Assert.AreEqual(2, calculator.result);
+        }
+
+        [TestMethod()]
+        public void AddIntDoubleTest()
+        {
+            Calc calculator = new Calc();
+            int a = 1;
+            double b = 2.02;
+            double c = calculator.Add(a, b);
+
+            Assert.AreEqual(3.02, c);
+            Assert.AreEqual(3.02, calculator.result);
+        }
+
+        [TestMethod()]
+        public void AddArrayDoubleTest()
+        {
+            Calc calculator = new Calc();
+            double a = 1.01;
+            double b = 2.00;
+            double[] values = { a, b };
+
+            double c = calculator.Add(values);
+
+            Assert.AreEqual(3.01, c);
+            Assert.AreEqual(3.01, calculator.result);
+        }
+
+        [TestMethod()]
+        public void AddArrayIntTest()
+        {
+            Calc calculator = new Calc();
+            int[] values = { 1, 2 };
+
+            int c = calculator.Add(values);
+
+            Assert.AreEqual(3, c);
+            Assert.AreEqual(3, calculator.result);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Calc calculator = new Calc();
+
+            double c = calculator.Divide(2, 1);
+
+            Assert.AreEqual(2, c);
+            Assert.AreEqual(2, calculator.result);
+        }
+    }
+}
---
+++ Subtract/Subtract.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Ops;
+
+namespace Subtract
+{
+    public interface ISubtract
+    {
+        public dynamic Subtract(dynamic a, dynamic b);
+    }
+
+    public class Subtracting : ISubtract
+
+    {
+        public dynamic result;
+
+        public dynamic Subtract(dynamic a, dynamic b)
+        {
+            result = Subtraction.Difference(a, b);
+            return result;
+        }
+    }
+}
